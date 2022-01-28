7b57cfb7fe7b427461d56a91440a5afc22949ccd
ishar <isharjain19@gmail.com>
2022-01-29 02:05:12 +0530

    fixed map refresh issue

--- a.js
+++ a.js
@@ -10,6 +10,10 @@ fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_2HZsSF5M7q8JEusPfCHHN
       const long = info.location.lng;
       console.log(lat)
       console.log('cheating')
+      var container = L.DomUtil.get('map');
+if(container != null){
+container._leaflet_id = null;
+}
     var map = new L.Map('map', {
   center: new L.LatLng(lat,long),
   zoom: 15
@@ -23,11 +27,9 @@ layer = new L.TileLayer(tileUrl, {maxZoom: 18});
 map.addLayer(layer);
 var marker = L.marker([lat,long]).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>This is marker").openPopup();
-map.destroy;
 })
 
 document.getElementById("refresh").addEventListener("onClick",function(e) {
-    window.location.reload();
 })
     
 
